05979c7eaaf31a5d38757b6700ad062b1fd8f00d
fabricio-26 <fabricioohh@gmail.com>
2020-09-23 15:09:28 -0300

    Update

--- js/functions.js
+++ js/functions.js
@@ -21,10 +21,15 @@ $(function(){
             if(cliked == false){
                 $('.cicle').css('margin-left','35px');
                 $('.cicle').css('transition','0.5s');
+                $('body').css('background','white');
+                $('.button').css('background','rgb(18, 18, 20)');
+                
                 cliked = true;  
             }else{
                 cliked = false;
                 $('.cicle').css('margin-left','0px');
+                $('body').css('background','rgb(18, 18, 20)');
+                $('.button').css('background','#ccc');
             }
                 
             
