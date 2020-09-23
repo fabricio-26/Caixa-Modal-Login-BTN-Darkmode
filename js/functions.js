0ae1b85d6230ee411bdded442176e86de224cad9
fabricio-26 <fabricioohh@gmail.com>
2020-09-23 14:26:23 -0300

    versao 0.6

--- js/functions.js
+++ js/functions.js
@@ -12,4 +12,23 @@ $(function(){
             $('input[type=button]').fadeIn()
         })
     }
+
+    var cliked;
+    btnDarkmode()
+    function btnDarkmode(){
+        cliked = false;
+        $('.button').click(function(){
+            if(cliked == false){
+                $('.cicle').css('margin-left','35px');
+                $('.cicle').css('transition','0.5s');
+                cliked = true;  
+            }else{
+                cliked = false;
+                $('.cicle').css('margin-left','0px');
+            }
+                
+            
+            
+        })
+    }
 });
